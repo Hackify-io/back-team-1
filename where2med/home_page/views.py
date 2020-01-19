47d7e064e01f2e8decaece6629618247687a0dae
PaperMonoid <daniel.santiago.aguila.torres@gmail.com>
2020-01-18 20:06:30 -0800

    fix: error

--- where2med/home_page/views.py
+++ where2med/home_page/views.py
@@ -5,6 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import serializers
 from rest_framework.decorators import (
     api_view,
     authentication_classes,
@@ -19,17 +20,20 @@ def index(request):
 
     return render(request, "home_page/index.html", {})
 
+
 def list_centers(city, treatment):
     return MedicalCenter.objects.filter(city__icontains=city)
     # medical_centers_by_services = MedicalCenterService.objects.filter(service__icontains=treatment)
     # for medical_centers_by_service in medical_centers_by_services:
-    #     if medical_centers.contains() 
-    # return 
+    #     if medical_centers.contains()
+    # return
+
 
 class MedicalCenterSerializer(serializers.ModelSerializer):
     class Meta:
         model = MedicalCenter
         fields = ["name", "address", "rate"]
+
 
 @api_view(["GET"])
 @authentication_classes([TokenAuthentication])
@@ -42,4 +46,3 @@ def search(request):
     serializer = MedicalCenterSerializer(centers)
 
     return Response(serializer.data)
-
